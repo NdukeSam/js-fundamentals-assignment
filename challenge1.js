f91840b495e11dd95197dafb948c4e56e9a80029
Nduke Sam <diamondsam33@gmail.com>
2025-12-18 17:17:17 +0100

    challenge 1 done

---
+++ challenge1.js
@@ -0,0 +1,7 @@
+const fullName = "Nduke-Abasi Sam"
+const age = 26
+const favouriteProgrammingLanguage = "JavaScript"
+const isStudent = false
+const currentYear = new Date().getFullYear()
+
+console.log(`Hi there! My name is ${fullName}. I am ${age} years old and my favourite programming language is ${favouriteProgrammingLanguage}. It is ${isStudent} that I am a student. The current year is ${currentYear}.`)
