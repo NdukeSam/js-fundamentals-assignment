8c2436d690b57e92a1d32342b35966f2d4d98e74
Nduke Sam <diamondsam33@gmail.com>
2025-12-19 15:49:19 +0100

    merged appendDisplay

--- challenge2.js
+++ challenge2.js
@@ -2,8 +2,19 @@
 
 const display = document.getElementById('display');
 
+
+// I've merged the function to prevent consecutive operators
 function appendToDisplay(input) {
+    const operators = ['+', '-', '*', '/'];
+    const lastChar = display.value[display.value.length - 1];
+    
+    if (operators.includes(input) && operators.includes(lastChar)) {
+        return;
+    }
+
     display.value += input;
+    console.log('Button pressed:', input)
+    console.log(display.value);
 }
 
 function clearDisplay () {
@@ -19,14 +30,4 @@ function calculate() {
     }
 }
 
-//  this function prevents consecutive operators
-function appendToDisplay(input) {
-    const operators = ['+', '-', '*', '/'];
-    const lastChar = display.value[display.value.length - 1];
-    
-    if (operators.includes(input) && operators.includes(lastChar)) {
-        return;
-    }
-    
-    display.value += input;
-}
+
